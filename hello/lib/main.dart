e73e823fa7299cff44509760f189b4f1efe1f8fe
m_rifqi <muhammad.rifqi@bankraya.co.id>
2023-06-27 13:49:38 +0700

    extarct widget

--- hello/lib/main.dart
+++ hello/lib/main.dart
@@ -38,15 +38,16 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     return Scaffold(
       body: Column(
         children: [
-          Text('A random idea:'),
-          Text('this is ${appState.current.asLowerCase}'),
+          Text('A random AWESOME idea:'),
+          Text('this is ${pair.asLowerCase}'),
           ElevatedButton(
               onPressed: () {
-                print('Button pressed!');
+                appState.getNext();
               },
               child: Text('Next'))
         ],
